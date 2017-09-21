87d1339ccda5cf6389248fa3ede88a87322f55ca
yu zhang <alexzhangpro@gmail.com>
2017-09-21 08:46:10 -0400

    script for testing during development

---
+++ test.m
@@ -0,0 +1,23 @@
+% test the code for a certain element
+% each section below is used for testing one element
+%%
+clear
+run umesh_local_info.m
+run node_info.m
+run elm_coords_info.m
+run y_local_info.m
+elm_if( coord27_0, coord27_1, yl27_0, yl27_1, uml27_0, uml27_1 )
+%%
+clear
+run umesh_local_info.m
+run node_info.m
+run elm_coords_info.m
+run y_local_info.m
+elm_if( coord20_0, coord20_1, yl20_0, yl20_1, uml20_0, uml20_1 )
+%%
+clear
+run umesh_local_info.m
+run node_info.m
+run elm_coords_info.m
+run y_local_info.m
+elm_if( coord51_0, coord51_1, yl51_0, yl51_1, uml51_0, uml51_1 )
